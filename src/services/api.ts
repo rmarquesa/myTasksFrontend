df0292f2dfd549cb57b775121760c902bdd69964
Rodrigo Marques Alves <rodrigomarquesalves@gmail.com>
2022-10-23 21:14:30 +0100

    AJust variables, project name

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
+const MYTASKS_BACKEND_HOST = process.env.MYTASKS_BACKEND_HOST
+const MYTASKS_BACKEND_PORT = process.env.MYTASKS_BACKEND_PORT
+
 const api = axios.create({
-  baseURL: "http://localhost:3333"
+  baseURL: `http://${MYTASKS_BACKEND_HOST}:${MYTASKS_BACKEND_PORT}`
 })
 
 export default api
