88b7769317272f60cdffb12fad40a21bd707d85c
Rodrigo Marques Alves <rodrigomarquesalves@gmail.com>
2022-10-24 18:06:55 +0100

    Update Backend URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,7 @@
 import axios from 'axios'
 
-const HOST = process.env.REACT_APP_HOST_API_BACKEND
-const PORT = process.env.REACT_APP_PORT_API_BACKEND
-
 const api = axios.create({
-  baseURL: `http://${HOST}:${PORT}`
+  baseURL: `${process.env.REACT_APP_BACKEND_URL}`
 })
 
 export default api
